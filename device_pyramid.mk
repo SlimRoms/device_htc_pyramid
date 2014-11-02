a44de82c7b559ec615344a3a1f89c222a201ee30
Dimitris Mantzouranis <d3xter93@gmail.com>
2014-11-02 02:36:04 +0100

    Pyramid: USB OTG support
    Change-Id: I631a858941712b8cae5493ddf5a185313d92c2aa

--- device_pyramid.mk
+++ device_pyramid.mk
@@ -91,7 +91,9 @@ PRODUCT_COPY_FILES += \
 
 # Permissions
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml
 
 # Bluetooth firmware
 $(call inherit-product, device/htc/msm8660-common/bcm_hcd.mk)
